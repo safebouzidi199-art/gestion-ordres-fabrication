23918143ac1fab11e7ba2acc2b07f542b57e666e
safebouzidi199-art <safebouzidi199@gmail.com>
2026-05-16 19:24:51 +0300

    feat(deploy): add GCP Cloud Run deployment config - fix CORS OPTIONS preflight, add environment files with Cloud Run backend URL, remove nginx proxy block

--- frontend/src/app/services/machine.service.ts
+++ frontend/src/app/services/machine.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class MachineService {
-  private api = 'http://localhost:8080/machines';
+  private api = `${environment.apiUrl}/machines`;
   constructor(private http: HttpClient) {}
 
   getAll() { return this.http.get<any[]>(this.api); }
